b91ae2644b75a04f7a51fc9a9d3bd715837c3911
Matias <ethan.aliaga@gmail.com>
2025-05-09 12:37:13 -0500

    Primer commit

---
+++ src/app/AquaConecta/requests/components/water-request/water-request.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-water-request',
+  imports: [],
+  templateUrl: './water-request.component.html',
+  styleUrl: './water-request.component.css'
+})
+export class WaterRequestComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {WaterRequestComponent} from './AquaConecta/requests/components/water-request/water-request.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+  {path: 'requests', component:WaterRequestComponent,}
+];
+
+
